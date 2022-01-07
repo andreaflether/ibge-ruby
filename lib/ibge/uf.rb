f64ff7dd7c70212f458694820c2b8608f8625597
Andréa Alencar <andreaflether@gmail.com>
2022-01-06 21:59:37 -0300

    [feature] serviços referentes à UF

--- lib/ibge/uf.rb
+++ lib/ibge/uf.rb
@@ -1,11 +1,36 @@
+require 'rest-client'
+require 'json'
+
 module IBGE
   class UF
-    attr_accessor :id, :nome, :regiao
+    attr_accessor :id, :nome, :sigla, :regiao
 
     def initialize(options = {})
       @id     = options['id']
       @nome   = options['nome']
+      @sigla  = options['sigla']
       @regiao = Regiao.new(options['regiao'])
+    end
+
+    def self.obter_ufs
+      resposta = RestClient.get("#{BASE_URL}/estados")
+
+      ufs = JSON.parse(resposta.body)
+      ufs.map { |uf| UF.new(uf) }
+    end
+
+    def self.uf_por_identificador(uf)
+      resposta = RestClient.get("#{BASE_URL}/estados/#{uf}")
+
+      uf = JSON.parse(resposta.body)
+      UF.new(uf)
+    end
+
+    def self.ufs_por_regiao(regiao)
+      resposta = RestClient.get("#{BASE_URL}/regioes/#{regiao}/estados")
+
+      ufs = JSON.parse(resposta.body)
+      ufs.map { |uf| UF.new(uf) }
     end
   end
 end

ef75f31a202a7e1c8af658ad5124871e153b2b06
Andréa Alencar <andreaflether@gmail.com>
2022-01-06 22:25:06 -0300

    [add] documentação e melhorias no módulo de UF

--- lib/ibge/uf.rb
+++ lib/ibge/uf.rb
@@ -12,6 +12,13 @@ module IBGE
       @regiao = Regiao.new(options['regiao'])
     end
 
+    # Obtém o conjunto de Unidades da Federação do Brasil.
+    #
+    # @example
+    #           ufs = IBGE::UF.obter_ufs
+    #           ufs.first.class #=> IBGE::UF
+    #           ufs.first.nome #=> "Acre"
+    #           ufs.map(&:sigla) #=> ['AC', 'AM', 'AP'...]
     def self.obter_ufs
       resposta = RestClient.get("#{BASE_URL}/estados")
 
@@ -19,18 +26,48 @@ module IBGE
       ufs.map { |uf| UF.new(uf) }
     end
 
+    # Obtém o conjunto de Unidades da Federação do Brasil a partir dos respectivos identificadores.
+    # Pode ser informado a sigla, identificador (ID) ou um array de siglas/identificadores.
+    #
+    # @param uf [String, Integer, Array]
+    # @return [UF, Array<UF>]
+    #
+    # @example
+    #           uf = IBGE::UF.uf_por_identificador('CE')
+    #           uf.class #=> IBGE::UF
+    #           uf = IBGE::UF.ufs_por_identificador(['BA', 'CE'])
+    #           uf.class #=> Array
     def self.uf_por_identificador(uf)
+      uf       = formatar(uf)
       resposta = RestClient.get("#{BASE_URL}/estados/#{uf}")
 
       uf = JSON.parse(resposta.body)
       UF.new(uf)
     end
 
+    # Obtém o conjunto de Unidades da Federação do Brasil a partir dos identificadores das regiões.
+    # Pode ser informado a sigla, identificador (ID) ou um array de siglas/identificadores.
+    #
+    # @param regiao [String, Integer, Array]
+    # @return [Array<UF>]
+    #
+    # @example
+    #           ufs = IBGE::UF.ufs_por_regiao('NE')
+    #           ufs.first.nome #=> "Maranhão"
+    #           ufs = IBGE::UF.ufs_por_regiao(['NE', 'N'])
+    #           ufs.map(&:nome) #=> ['Ceará', 'Amazonas'...]
     def self.ufs_por_regiao(regiao)
+      regiao   = formatar(regiao)
       resposta = RestClient.get("#{BASE_URL}/regioes/#{regiao}/estados")
 
       ufs = JSON.parse(resposta.body)
       ufs.map { |uf| UF.new(uf) }
     end
+
+    private
+
+    def formatar(parametro)
+      parametro.is_a?(Array) ? parametro.join('|') : parametro
+    end 
   end
 end

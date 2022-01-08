e74cc8124b55d56f3ca3a9fb6cd24688ff6b57b5
Andréa Alencar <andreaflether@gmail.com>
2022-01-07 21:04:36 -0300

    [feature] serviços referentes à Países

--- lib/ibge.rb
+++ lib/ibge.rb
@@ -1,3 +1,6 @@
+require 'rest-client'
+require 'json'
+
 module IBGE
   BASE_URL = "https://servicodados.ibge.gov.br/api/v1/localidades"
 
@@ -7,5 +10,12 @@ module IBGE
   autoload :Microrregiao, 'ibge/microrregiao'
   autoload :Municipio,    'ibge/municipio'
   autoload :Regiao,       'ibge/regiao'
+  autoload :Pais,         'ibge/pais'
   autoload :UF,           'ibge/uf'
+
+  private
+  
+  def formatar(parametro)
+    parametro.is_a?(Array) ? parametro.join('|') : parametro
+  end 
 end
---
+++ lib/ibge/pais.rb
@@ -0,0 +1,44 @@
+module IBGE
+  class Pais
+    attr_accessor :id, :nome, :regiao_intermediaria, :sub_regiao
+
+    def initialize(options = {})
+      @id                   = options['id']
+      @nome                 = options['nome']
+      @regiao_intermediaria = options['regiao-intermediaria']
+      @sub_regiao           = options['sub-regiao']
+    end
+
+    # Obtém o conjunto de países.
+    # 
+    # @example
+    #           paises = IBGE::Pais.obter_paises
+    #           paises.first.nome #=> "Afeganistão"
+    #           paises.first.class #=> IBGE::Pais
+    def self.obter_paises
+      resposta = RestClient.get("#{BASE_URL}/paises")
+      paises   = JSON.parse(resposta.body)
+
+      paises.map { |pais| Pais.new(pais) }
+    end
+
+    # Obtém o conjunto de países a partir dos respectivos identificadores.
+    # Pode ser informado o identificador (ID) ou um array de identificadores.
+    #
+    # @param paises[String, Array]
+    # @return [Array<Pais>]
+    #
+    # @example
+    #           pais = IBGE::Pais.paises_por_identificador(76)
+    #           pais.nome #=> "Brasil"
+    #           paises = IBGE::Pais.paises_por_identificador([32, 76])
+    #           paises.map(&:nome) #=> ["Argentina", "Brasil"]
+    def self.paises_por_identificador(paises)
+      paises   = formatar(paises)
+      resposta = RestClient.get("#{BASE_URL}/paises/#{paises}")
+      paises   = JSON.parse(resposta.body)
+
+      paises.map { |pais| Pais.new(pais) }
+    end
+  end
+end

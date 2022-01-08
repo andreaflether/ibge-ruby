5600dbc11bccca8c1c9a3c8c85cbffc68a64a119
Andréa Alencar <andreaflether@gmail.com>
2022-01-08 10:47:40 -0300

    [add] config do rspec/vcr/simplecov

---
+++ spec/spec_helper.rb
@@ -0,0 +1,27 @@
+require 'ibge'
+require 'vcr'
+require 'webmock/rspec'
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec'
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/vcr_cassettes'
+  c.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.order = :random
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+
+    mocks.verify_partial_doubles = true
+  end
+end

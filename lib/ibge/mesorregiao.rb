3379cb326691cc6bf410a5c0a004c59ee1d2d227
Andréa Alencar <andreaflether@gmail.com>
2022-01-08 23:10:41 -0300

    [feature] serviços referentes à Mesorregiões

--- lib/ibge/mesorregiao.rb
+++ lib/ibge/mesorregiao.rb
@@ -7,5 +7,44 @@ module IBGE
       @nome = options['nome']
       @uf   = IBGE::UF.new(options['UF'])
     end
+
+    def self.obter_mesorregioes
+      resposta = RestClient.get("#{BASE_URL}/mesorregioes")
+
+      tratar_retorno(resposta)
+    end
+
+    def self.mesorregioes_por_uf(ufs)
+      ufs      = IBGE.formatar(ufs)
+      resposta = RestClient.get("#{BASE_URL}/estados/#{ufs}/mesorregioes")
+
+      tratar_retorno(resposta)
+    end
+
+    def self.mesorregioes_por_id(ids)
+      ids      = IBGE.formatar(ids)
+      resposta = RestClient.get("#{BASE_URL}/mesorregioes/#{ids}")
+      
+      tratar_retorno(resposta)
+    end
+
+    def self.mesorregioes_por_regiao(regioes)
+      regioes  = IBGE.formatar(regioes)
+      resposta = RestClient.get("#{BASE_URL}/regioes/#{regioes}/mesorregioes")
+
+      tratar_retorno(resposta)
+    end
+
+    private
+
+    def self.tratar_retorno(resposta)
+      mesorregioes = JSON.parse(resposta.body)
+
+      if mesorregioes.is_a?(Array) 
+        mesorregioes.map { |mesorregiao| Mesorregiao.new(mesorregiao) }
+      else
+        Mesorregiao.new(mesorregioes)
+      end 
+    end
   end
 end

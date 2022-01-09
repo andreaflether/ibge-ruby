e5c2c42c9ff16e55a012e286d9aba85b9bf0ffa6
Andréa Alencar <andreaflether@gmail.com>
2022-01-09 00:08:25 -0300

    [add] documentação no módulo IBGE::Mesorregiao

--- lib/ibge/mesorregiao.rb
+++ lib/ibge/mesorregiao.rb
@@ -8,12 +8,31 @@ module IBGE
       @uf   = IBGE::UF.new(options['UF'])
     end
 
+    # Obtém o conjunto de mesorregiões do Brasil.
+    #
+    # @return [Array<Mesorregiao>]
+    #
+    # @example
+    #           mesorregioes = IBGE::Mesorregiao.obter_mesorregioes
+    #           mesorregioes.first.nome #=> "Madeira-Guaporé"
     def self.obter_mesorregioes
       resposta = RestClient.get("#{BASE_URL}/mesorregioes")
 
       tratar_retorno(resposta)
     end
 
+    # Obtém o conjunto de mesorregiões do Brasil a partir dos identificadores das Unidades da Federação.
+    # Pode ser informada a sigla, identificador (ID) ou um array de siglas/identificadores. 
+    # 
+    # @param ufs [String, Integer, Array]
+    # @return [Array<Mesorregiao>]
+    #
+    # @example
+    #           mesorregioes = IBGE::Mesorregiao.mesorregioes_por_uf('CE')
+    #           mesorregioes.first.nome #=> "Noroeste Cearense"
+    #
+    #           IBGE::Mesorregiao.mesorregioes_por_uf(['BA', 'CE'])
+    #           IBGE::Mesorregiao.mesorregioes_por_uf(23) 
     def self.mesorregioes_por_uf(ufs)
       ufs      = IBGE.formatar(ufs)
       resposta = RestClient.get("#{BASE_URL}/estados/#{ufs}/mesorregioes")
@@ -21,6 +40,17 @@ module IBGE
       tratar_retorno(resposta)
     end
 
+    # Obtém o conjunto de mesorregiões do Brasil a partir dos respectivos identificadores.
+    # Pode ser informado um único ID ou um array de IDs.
+    #
+    # @param ids [String, Integer, Array]
+    # @return [Mesorregiao, Array<Mesorregiao>]
+    #
+    # @example
+    #           mesorregiao = IBGE::Mesorregiao.mesorregioes_por_id(2303)
+    #           mesorregiao.nome #=> "Metropolitana de Fortaleza"
+    #
+    #           mesorregioes = IBGE::Mesorregiao.mesorregioes_por_id([2303, 2304])
     def self.mesorregioes_por_id(ids)
       ids      = IBGE.formatar(ids)
       resposta = RestClient.get("#{BASE_URL}/mesorregioes/#{ids}")
@@ -28,6 +58,18 @@ module IBGE
       tratar_retorno(resposta)
     end
 
+    # Obtém o conjunto de mesorregiões do Brasil a partir dos identificadores das regiões.
+    # Pode ser informada a sigla, identificador (ID) ou um array de siglas/identificadores.
+    #
+    # @param regioes [String, Integer, Array]
+    # @return [Array<Mesorregiao>]
+    #
+    # @example
+    #           mesorregioes = IBGE::Mesorregiao.mesorregioes_por_regiao('NE')
+    #           mesorregioes.first.nome #=> "Norte Maranhense"
+    #
+    #           IBGE::Mesorregiao.mesorregioes_por_regiao(['NE', 'N'])
+    #           IBGE::Mesorregiao.mesorregioes_por_regiao([1, 2])
     def self.mesorregioes_por_regiao(regioes)
       regioes  = IBGE.formatar(regioes)
       resposta = RestClient.get("#{BASE_URL}/regioes/#{regioes}/mesorregioes")

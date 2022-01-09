66721428a07cb883ee8f32dc0c91dab62e625386
Andréa Alencar <andreaflether@gmail.com>
2022-01-09 11:20:33 -0300

    [feature] serviços referentes à Microrregiões

--- lib/ibge/microrregiao.rb
+++ lib/ibge/microrregiao.rb
@@ -7,5 +7,51 @@ module IBGE
       @nome        = options['nome']
       @mesorregiao = Mesorregiao.new(options['mesorregiao'])
     end
+
+    def self.obter_microrregioes
+      resposta = RestClient.get("#{BASE_URL}/microrregioes")
+
+      tratar_retorno(resposta)
+    end
+    
+    def self.microrregioes_por_id(ids)
+      ids      = IBGE.formatar(ids)
+      resposta = RestClient.get("#{BASE_URL}/microrregioes/#{ids}")
+
+      tratar_retorno(resposta)
+    end
+    
+    def self.microrregioes_por_uf(ufs)
+      ufs      = IBGE.formatar(ufs)
+      resposta = RestClient.get("#{BASE_URL}/estados/#{ufs}/microrregioes")
+
+      tratar_retorno(resposta)
+    end
+
+    def self.microrregioes_por_regiao(regioes)
+      regioes  = IBGE.formatar(regioes)
+      resposta = RestClient.get("#{BASE_URL}/regioes/#{regioes}/microrregioes")
+
+      tratar_retorno(resposta)
+    end
+
+    def self.microrregioes_por_mesorregiao(mesorregioes)
+      mesorregioes = IBGE.formatar(mesorregioes)
+      resposta     = RestClient.get("#{BASE_URL}/mesorregioes/#{mesorregioes}/microrregioes")
+      
+      tratar_retorno(resposta)
+    end
+
+    private
+
+    def self.tratar_retorno(resposta)
+      microrregioes = JSON.parse(resposta.body)
+
+      if microrregioes.is_a?(Array) 
+        microrregioes.map { |microrregiao| Microrregiao.new(microrregiao) }
+      else
+        Microrregiao.new(microrregioes)
+      end 
+    end
   end
 end

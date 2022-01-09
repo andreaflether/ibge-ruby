20ce3d851946e78005547b7ebea399ee657d4c4f
Andréa Alencar <andreaflether@gmail.com>
2022-01-09 20:57:56 -0300

    melhorias e adições no arquivo principal (lib/ibge)

--- lib/ibge.rb
+++ lib/ibge.rb
@@ -3,14 +3,16 @@ require 'json'
 require 'ibge/utils'
 
 module IBGE
-  BASE_URL = "https://servicodados.ibge.gov.br/api/v1/localidades"
+  BASE_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades'
 
-  autoload :Distrito,     'ibge/distrito'
-  autoload :Estado,       'ibge/estado'
-  autoload :Mesorregiao,  'ibge/mesorregiao'
-  autoload :Microrregiao, 'ibge/microrregiao'
-  autoload :Municipio,    'ibge/municipio'
-  autoload :Regiao,       'ibge/regiao'
-  autoload :Pais,         'ibge/pais'
-  autoload :UF,           'ibge/uf'
+  autoload :Distrito,            'ibge/distrito'
+  autoload :Estado,              'ibge/estado'
+  autoload :Mesorregiao,         'ibge/mesorregiao'
+  autoload :Microrregiao,        'ibge/microrregiao'
+  autoload :Municipio,           'ibge/municipio'
+  autoload :Regiao,              'ibge/regiao'
+  autoload :RegiaoImediata,      'ibge/regiao_imediata'
+  autoload :RegiaoIntermediaria, 'ibge/regiao_intermediaria'
+  autoload :Pais,                'ibge/pais'
+  autoload :UF,                  'ibge/uf'
 end

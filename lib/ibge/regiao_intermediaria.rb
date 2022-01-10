54e3cf08172de13e69f250611158d185e9eb1bc7
Andréa Alencar <andreaflether@gmail.com>
2022-01-09 22:57:45 -0300

    [feature] serviços referentes à Regiões Intermediárias

--- lib/ibge/regiao_intermediaria.rb
+++ lib/ibge/regiao_intermediaria.rb
@@ -8,12 +8,43 @@ module IBGE
       @uf   = UF.new(options['UF'])
     end
 
-    def self.obter_regioes_intermediarias; end
+    def self.obter_regioes_intermediarias
+      resposta = RestClient.get("#{BASE_URL}/regioes-intermediarias")
 
-    def self.regioes_intermediarias_por_id(ids); end
+      tratar_retorno(resposta)
+    end
 
-    def self.regioes_intermediarias_por_uf(ufs); end
+    def self.regioes_intermediarias_por_id(ids)
+      ids      = IBGE.formatar(ids)
+      resposta = RestClient.get("#{BASE_URL}/regioes-intermediarias/#{ids}")
+      
+      tratar_retorno(resposta)
+    end
 
-    def self.regioes_intermediarias_por_regiao(regioes); end
+    def self.regioes_intermediarias_por_uf(ufs)
+      ufs      = IBGE.formatar(ufs)
+      resposta = RestClient.get("#{BASE_URL}/estados/#{ufs}/regioes-intermediarias")
+      
+      tratar_retorno(resposta)
+    end
+
+    def self.regioes_intermediarias_por_regiao(regioes)
+      regioes  = IBGE.formatar(regioes)
+      resposta = RestClient.get("#{BASE_URL}/regioes/#{regioes}/regioes-intermediarias")
+      
+      tratar_retorno(resposta)
+    end
+
+    private
+
+    def self.tratar_retorno(resposta)
+      regioes = JSON.parse(resposta.body)
+
+      if regioes.is_a?(Array) 
+        regioes.map { |regiao| RegiaoIntermediaria.new(regiao) }
+      else
+        RegiaoIntermediaria.new(regioes)
+      end 
+    end
   end
 end

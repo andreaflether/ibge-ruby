f139ef963cee3d5cd0fa2d6b3d8bb4078c503b35
Andréa Alencar <andreaflether@gmail.com>
2022-01-24 19:57:13 -0300

    remoção do módulo Estado

--- lib/ibge.rb
+++ lib/ibge.rb
@@ -6,7 +6,6 @@ module IBGE
   BASE_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades'
 
   autoload :Distrito,            'ibge/distrito'
-  autoload :Estado,              'ibge/estado'
   autoload :Mesorregiao,         'ibge/mesorregiao'
   autoload :Microrregiao,        'ibge/microrregiao'
   autoload :Municipio,           'ibge/municipio'
